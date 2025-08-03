e744df9a26df3ec900b9034d8dff557c0a9a9bdf
Peter Sido <petersido123@gmail.com>
2025-08-03 16:41:32 +0100

    prod changed for dev

--- Blog/wsgi.py
+++ Blog/wsgi.py
@@ -1,5 +1,5 @@
 # c:/Users/Peter/Blog/Blog/wsgi.py |||
 import os
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Blog.settings.prod')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Blog.settings.dev')
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 def main():
     """Run administrative tasks."""
     # Default to development settings
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Blog.settings.prod')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Blog.settings.dev')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
