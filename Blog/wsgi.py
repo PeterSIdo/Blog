8dd3429de7a5768314921a373552e4647d1df3fe
PeterSIdo <petersido123@gmail.com>
2025-08-11 22:37:35 +0100

    deployed by Heroku extension

--- Blog/wsgi.py
+++ Blog/wsgi.py
@@ -1,5 +1,5 @@
 # c:/Users/Peter/Blog/Blog/wsgi.py |||
 import os
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Blog.settings.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Blog.settings.prod')
 application = get_wsgi_application()
